68603f99f5750b76310e2dc00fe992173fd2790d
Akulov Maksym <maxim.akulovka@gmail.com>
2021-12-16 13:10:46 +0200

    commit to the heroku

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -17,7 +17,7 @@ $('.header__burger').click(function () {
 var timeOut;
 
 function goUp() {
-    var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop); // Получаем максимальные значиния из двух значений, document.documentElement.scrollTop для кроссбраузерности
+    var top = Math.max(document.body.scrollTop, document.documentElement.scrollTop); // Получаем максимальные значения из двух значений, document.documentElement.scrollTop для кроссбраузерности
     if (top > 0) { // Если находимся внизу(или, к примеру, в середине экрана), то
         window.scrollBy(0, -100); // Поднятие вверх на 100px
         timeOut = setTimeout('goUp()', 30); // Каждые 30 милисекунд функция срабатывает и страница подымается вверх
