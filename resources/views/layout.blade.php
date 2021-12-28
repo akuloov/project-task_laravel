ea0bd99c05658b062d7ac02e324c75ff1f3bba95
Akulov Maksym <maxim.akulovka@gmail.com>
2021-12-28 22:17:48 +0200

    layout popup-menu

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,14 +19,81 @@
                     <a href="" class="categories__link">
                         <div class="categories__item">
                             <svg class="categories__icon categories__icon--animal">
-                                <use href="assets/images/icons.svg#icon-rodents"></use>
+                                <use href="assets/images/icons.svg#icon-dogs"></use>
                             </svg>
                             <span class="categories__text">Собаки</span>
-                            <svg class="categories__icon-vector">
+                            <svg class="categories__icon categories__icon-vector">
                                 <use href="assets/images/icons.svg#icon-vector"></use>
                             </svg>
                         </div>
                     </a>
+                    <a href="" class="categories__link">
+                        <div class="categories__item">
+                            <svg class="categories__icon categories__icon--animal">
+                                <use href="assets/images/icons.svg#icon-cats"></use>
+                            </svg>
+                            <span class="categories__text">Кошки</span>
+                            <svg class="categories__icon categories__icon-vector">
+                                <use href="assets/images/icons.svg#icon-vector"></use>
+                            </svg>
+                        </div>
+                    </a>
+                    <a href="" class="categories__link">
+                        <div class="categories__item">
+                            <svg class="categories__icon categories__icon--animal">
+                                <use href="assets/images/icons.svg#icon-birds"></use>
+                            </svg>
+                            <span class="categories__text">Птицы</span>
+                            <svg class="categories__icon categories__icon-vector">
+                                <use href="assets/images/icons.svg#icon-vector"></use>
+                            </svg>
+                        </div>
+                    </a>
+                    <a href="" class="categories__link">
+                        <div class="categories__item">
+                            <svg class="categories__icon categories__icon--animal">
+                                <use href="assets/images/icons.svg#icon-rodents"></use>
+                            </svg>
+                            <span class="categories__text">Грызуны</span>
+                            <svg class="categories__icon categories__icon-vector">
+                                <use href="assets/images/icons.svg#icon-vector"></use>
+                            </svg>
+                        </div>
+                    </a>
+                    <a href="" class="categories__link">
+                        <div class="categories__item">
+                            <svg class="categories__icon categories__icon--animal categories__icon--fill categories__icon--stroke-none">
+                                <use href="assets/images/icons.svg#icon-staar"></use>
+                            </svg>
+                            <span class="categories__text">Новинки</span>
+                        </div>
+                    </a>
+                    <a href="" class="categories__link">
+                        <div class="categories__item">
+                            <svg class="categories__icon categories__icon--animal categories__icon--fill categories__icon--stroke-none">
+                                <use href="assets/images/icons.svg#icon-percent"></use>
+                            </svg>
+                            <span class="categories__text">Акции</span>
+                        </div>
+                    </a>  
+                </div>
+                <div class="kinds">
+                    <div class="kinds__kind kind">
+                        <div class="kind__title">Корм</div>
+                        <a href="" class="kind__item">Сухой корм</a>
+                        <a href="" class="kind__item">Консервы</a>
+                        <a href="" class="kind__item">Ветеринарные диеты</a>
+                        <a href="" class="kind__item">Заменители молока</a>
+                        <a href="" class="kind__item kind__item--green">Смотреть все ></a>
+                    </div>
+                    <div class="kind">
+                        <div class="kind__title">Корм</div>
+                        <a href="" class="kind__item">Сухой корм</a>
+                        <a href="" class="kind__item">Консервы</a>
+                        <a href="" class="kind__item">Ветеринарные диеты</a>
+                        <a href="" class="kind__item">Заменители молока</a>
+                        <a href="" class="kind__item kind__item--green">Смотреть все ></a>
+                    </div>
                 </div>
             </div>
             @include("includes.popup-search")
