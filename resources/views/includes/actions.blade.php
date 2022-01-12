1cb81671e4346ba5b87df6c71fcdf3ec60e5bce6
Akulov Maksym <maxim.akulovka@gmail.com>
2022-01-12 12:08:10 +0200

    products-leftBlock

---
+++ resources/views/includes/actions.blade.php
@@ -0,0 +1,36 @@
+<div class="actions-wrapper">
+    <div class="actions">
+        <div class="action">
+            <a href="" class="action__link-img">
+                <svg class="action__img">
+                    <use href="assets/images/icons.svg#icon-action1"></use>
+                </svg>
+            </a>
+            <div class="action__link-text">Бесплатная доставка при заказе от 300 грн</div>
+        </div>
+        <div class="action">
+            <a href="" class="action__link-img">
+                <svg class="action__img">
+                    <use href="assets/images/icons.svg#icon-action2"></use>
+                </svg>
+            </a>
+            <div class="action__link-text">Акции и скидки среди 2400 товаров</div>
+        </div>
+        <div class="action">
+            <a href="" class="action__link-img">
+                <svg class="action__img">
+                    <use href="assets/images/icons.svg#icon-action3"></use>
+                </svg>
+            </a>
+            <div class="action__link-text">Более 100 магазинов по всей Украине</div>
+        </div>
+        <div class="action">
+            <a href="" class="action__link-img">
+                <svg class="action__img action__img--stroke" fill="none">
+                    <use href="assets/images/icons.svg#icon-action4"></use>
+                </svg>
+            </a>
+            <div class="action__link-text">Дисконтная программа ZooBonus.ua</div>
+        </div>
+    </div>
+</div>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -43,11 +43,30 @@
                     <div class="categories-int">(59)</div>
                 </div>
             </div>
-            <div class="auto-order">
-
+            <div class="products__auto-order auto-order">
+                <div class="auto-order__top">
+                    <div class="auto-order__checkbox checkbox-submit__checkbox">
+                        <svg class="checkbox-submit__img">
+                            <use href="assets/images/icons.svg#icon-done"></use>
+                        </svg>
+                    </div>
+                    <div class="auto-order__img">
+                        <svg class="auto-order__icon">
+                            <use href="assets/images/icons.svg#icon-reload"></use>
+                        </svg>
+                    </div>
+                    <a href="" class="auto-order__text">Автозаказ</a>
+                    <svg class="auto-order__question">
+                        <use href="assets/images/icons.svg#icon-question"></use>
+                    </svg>
+                </div>
+                <div class="auto-order__bottom">
+                    Экономия до 10% на заказах
+                </div>
             </div>
         </div>
     </div>
 </div>
+@include("includes.actions")
 @include("includes.about-us")
 @endsection
